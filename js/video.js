9fb47408a5a5f6b6256ecf000adf9c56c13104ec
Billy Dallimore <billy@sngtrkr.com>
2013-06-20 17:45:19 +0100

    Added DOM foundations in, and area mapping setup to show on specified time

--- js/video.js
+++ js/video.js
@@ -2,15 +2,9 @@ var dysonPlayer;
 var dysonVP;
 var experienceModule;
 var DV = {}
-var scene = 0;
-//text time array
-var tbegin = new Array(31,54);
-var tend = new Array(36,57);
-//loop time array
-var lbegin = new Array(6,14,24,44); //array of loop begin values
-var lend_value = new Array(8,18,28,53); //array of loop lend values
+var mbegin = new Array(15,20);
+var mend = new Array(19,24);
 var lend = [];
-var vidReady = 0;
 DV.videoTemplateLoad = function(experienceID) {
     console.log("INTERACTIVE VIDEO: TEMPLATE_LOADED")
     DV.dysonPlayer = brightcove.getExperience(experienceID);
@@ -19,7 +13,6 @@ DV.videoTemplateLoad = function(experienceID) {
 }
 DV.videoTemplateReady = function(evt) {
     console.log("INTERACTIVE VIDEO: TEMPLATE_READY");
-    vidReady = 1;
     DV.dysonVP = DV.dysonPlayer.getModule(APIModules.VIDEO_PLAYER)
     DV.dysonVP.setBackBufferCapacity(120);
     DV.dysonVP.setBufferCapacity(120);
@@ -27,86 +20,40 @@ DV.videoTemplateReady = function(evt) {
     DV.dysonVP.addEventListener(BCMediaEvent.PROGRESS, DV.onProgress);
     DV.dysonVP.addEventListener(BCMediaEvent.COMPLETE, DV.onComplete);
 }
+DV.onPlay = function(evt) {
+   document.getElementById("eventLog").innerHTML += "MEDIA EVENT: " + evt.type + " fired at position: " + evt.position + "<BR>";
+}
 DV.onProgress = function (evt) {    
     var dur = evt.duration;
     var pos = evt.position;
-    // Creates new array and loops through the end loop values to calcuate the difference between the video duration and the end value of the loop. This then provides the value to subtract from the video duration to reach the desired end loop value, which is required in the video loop logic.
-    for_loop(lend_value, lend, dur);
-    //LOOP GENERATOR
-    //checks there is an equal amount of begin and end values
-    if (equal_to(lbegin, lend_value)) 
+    //MAPPING GENERATOR
+    if (equal_to(mbegin, mend)) 
     {
-        //iterates through the each begin value and creates the desired loop point
-        for (var lp=0; lp < lbegin.length; lp++)
-        {   
-            if (greater_than(pos, dur - lend[lp]) && scene == lp) {
-                //checks if text elements have the animate class, if not adds it to enable fade in it at the correct time
-                var el = '.scene_' + Math.floor(lbegin[lp]);
-                show_text(el);
-                seek_vid(lbegin[lp]);
+        setTimeout(function() {
+            for (var map=0; map < mbegin.length;map++)
+            {
+                var el = '.map_' + Math.floor(mbegin[map]);
+                if (between(pos, mbegin[map], mend[map])) {
+                    add_map(el);
+                    map++;
+                } else {
+                    remove_map(el);
+                }
             }
-        }
+        }, 1000);
     } else {
-        console.log("WARNING: You do not have equal loop begin and end values!");
+        console.log("WARNING: You do not have equal mapping begin and end values!");
     }
-    //TEXT GENERATOR
-    if (equal_to(tbegin, tend)) 
-    {
-        for (var txt=0; txt < tbegin.length; txt++)
-        {
-            var el = '.popover_' + Math.floor(tbegin[txt]);
-            if (between(pos, tbegin[txt], tend[txt])) {
-                show_text(el);
-            } else {
-                hide_text(el);
-            }
-        }
-    } else {
-        console.log("WARNING: You do not have equal text begin and end values!");
-    }
-}
-//USER ACTIONS
-DV.next = function() {
-    //increases scene value by 1 upon clicking a CTA to remove current loop and trigger the next loop
-    scene++;
-    current_scene = scene+1;
-    return false;
 }
 DV.restart = function() {
     //Resets the scene variable back to 0 to trigger the first loop and sets the video position back to 0
     scene = 0;
     seek_vid(0);
-    return false;
-}
-DV.select_scene = function(location) {
-    if (location > 0) {
-        seek_vid(lend_value[location-1]);
-    } else {        
-        seek_vid(0);
-    }
-    scene = location;
+    document.getElementById("eventLog").innerHTML += "USER EVENT: <b>Journey reset<b/><br/>";
     return false;
 }
 DV.play = function() {
     DV.dysonVP.play();
-}
-DV.prev_scene = function() {
-    if (scene > 1) {
-        scene--;
-        previous_scene = lend_value[scene-1];
-        seek_vid(previous_scene);
-    } else {
-        scene--;
-        seek_vid(0);
-    }
-    return false;
-}
-DV.next_scene = function() {
-    if (scene < lbegin.length) {
-        seek_vid(lend_value[scene]);
-        scene++;
-    }
-    return false;
 }
 //HELPER FUNCTIONS
 between = function(x, min, max) {
@@ -115,17 +62,19 @@ between = function(x, min, max) {
 greater_than = function(x,min) {
     return x >= min;
 }
-show_text = function(elem) {
-    if ($jq(elem).hasClass('fadeOutUp')) 
-    {
-        $jq(elem).removeClass('fadeOutUp').show().addClass('fadeInDown');
+lesser_greater = function(x, min, max) {
+    return x < min && x > max;
+}
+add_map = function(elem) {
+    if ($('.mappings').children().length < 1) {
+        $('.mappings').append('<area alt=\"Cleaner head\" class=\"map_15\" coords=\"265,53,398,143\" href=\"#\" shape=\"rect\">');
+    } else {
+        return false;
     }
 }
-hide_text = function(elem) {
-    if ($jq(elem).hasClass('fadeInDown')) 
-    {
-        $jq(elem).removeClass('fadeInDown').addClass('fadeOutUp').one('webkitAnimationEnd animationend', function() { });
-    }
+remove_map = function(elem) {
+    $('.mappings').children().remove();
+    console.log("NOOOO");
 }
 for_loop = function(time_val, time, duration) {
     for (var i=0; i < time_val.length; i++)
--- src/coffee/application.coffee
+++ src/coffee/application.coffee
@@ -1,10 +1,4 @@
 $(document).ready ->
-  $('.static_trigger').click ->
-    $('.video_wrapper').removeClass('inactive').show().addClass 'activated'
-    $('.static_wrapper').removeClass('activated').hide().addClass 'inactive'
-    setInterval(playTab, 200);
-
-
   $('.scene').click ->
     if !$(@).hasClass "disabled"
       $(@).addClass "disabled"
@@ -22,94 +16,13 @@ $(document).ready ->
     txtAnimation()
     changePin(1)
 
-  # Trigger the interactive video by clicking the tab
-  current_tab = $('.tab_trigger').parent().parent().index()
-  $('li.act-'+current_tab).click ->
-    setInterval(playTab, 200);
-
-  # Render the static introduction content again upon click the tab
-  static_current_tab = $('.static_tab_trigger').parent().parent().index()
-  $('li.act-'+static_current_tab).click ->
-    $('.static_wrapper').removeClass('inactive').show().addClass 'activated'
-    $('.video_wrapper').removeClass('activated').hide().addClass 'inactive'
-    $('#pins').hide().addClass 'inactive'
-
-
-
-  $(document).on "click", ".icon-chevron-left", ->
-    DV.prev_scene()
-    hideArrows()
-    txtAnimation()
-    removeDisabled("scene")
-    changePin(0)
-    
-
-  $(document).on "click", ".icon-chevron-right", ->
-    DV.next_scene()
-    hideArrows()
-    txtAnimation()
-    removeDisabled("scene")
-    changePin(1)
-
-  $(document).on "click", ".icon-circle", ->
-    pinNumber = $(@).index()
-    $('.active-pill').removeClass 'active-pill'
-    $(@).addClass 'active-pill'
-    removeDisabled("scene")
-    txtAnimation()
-    DV.select_scene(pinNumber)
-    hideArrows()
-
-  if lbegin.length is lend_value.length
-    i = 0
-    # Creates the pins dynamically from the timing array
-    while i < lbegin.length
-      $('#pins').append '<i class="icon-circle"></i>'
-      i++
-    if lbegin.length > 1
-      # Create and position the scene navigation
-      $('.video_wrapper').prepend('<i class="icon-chevron-left"></i>').append '<i class="icon-chevron-right"></i>'
-      vheight = $('.video_wrapper').height()
-      iheight = vheight/2
-      # Temporary: The +60 is for the buffer above the video in the prototype and the -15 is half the height of the chevron icons
-      $('.video_wrapper i').css "top", iheight-15
-
-  $('#pins').find('.icon-circle:first-child').addClass 'active-pill'
-
-# HELPER FUNCTIONS
-changePin = (direction) ->
-  active = $(".active-pill").removeClass("active-pill")
-  if direction is 1
-    if active.next() and active.next().length
-        active.next().addClass "active-pill"
-    else
-      active.siblings(":first").addClass "active-pill"
-  else
-    if active.prev() and active.prev().length
-        active.prev().addClass "active-pill"
-    else
-      active.siblings(":first").addClass "active-pill"
-
-hideArrows = ->
-    if scene is 0
-        $('.icon-chevron-left').hide()
-        $('.icon-chevron-right').show()
-    else if scene is 3
-        $('.icon-chevron-right').hide()
-        $('.icon-chevron-left').show()
-    else 
-      $('.icon-chevron-left').show()
-      $('.icon-chevron-right').show()
-
-playTab = ->
-  if vidReady is 1
+  $('.play').click ->
     DV.play()
-    hideArrows()
-    txtAnimation()
-    $('#pins').removeClass('inactive').show().addClass 'activated'
-    vidReady++
-  else
+    $(@).parent().hide()
+  $('.map_4').click ->
+    DV.play()
     return false
+
 
 txtAnimation = ->
   return $('.scene_trigger').removeClass("fadeInDown").addClass("fadeOutUp").one "webkitAnimationEnd animationend", ->
